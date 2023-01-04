ddbe747c12ecd4310017aa5188cc9430b8c43857
Talmon Guedes <Talmon.Guedes@hotmail.com>
2023-01-04 10:14:00 +0000

    save

---
+++ Assets/Scripts/WaitingRoom.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class WaitingRoom : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if(PhotonNetwork.CurrentRoom.PlayerCount == 2)
+        {
+            Debug.Log("BILLYYY");
+        }
+    }
+}

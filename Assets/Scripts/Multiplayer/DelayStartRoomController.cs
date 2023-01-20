52353d325d83ef2c018f3199115cfbf94fb4f44f
TalmonG <118438762+TalmonG@users.noreply.github.com>
2023-01-20 11:50:45 +0000

    11:14

---
+++ Assets/Scripts/Multiplayer/DelayStartRoomController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class DelayStartRoomController : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private int multiplayerSceneIndex;
+
+    public override void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public override void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined Room");
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Starting Game");
+            PhotonNetwork.LoadLevel(multiplayerSceneIndex);
+        }
+    }
+}
